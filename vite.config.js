682d69d53588df5d1d1b79885576bc7703555d99
xovrashok <juliaruda033@gmail.com>
2026-07-13 00:52:08 +0300

    Update code

--- vite.config.js
+++ vite.config.js
@@ -39,7 +39,7 @@ export default defineConfig(({ command }) => {
     },
     plugins: [
       injectHTML(),
-      FullReload(['.//**.html']),
+      FullReload(['.//*.html']),
       SortCss({
         sort: 'mobile-first',
       }),
